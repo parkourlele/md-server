60ae8ed805c067c30377a15e1c4c5baf86205b38
parkourlele <2530778901@qq.com>
2025-09-29 15:27:36 +0800

    后端创建user模块，调通接口

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import {PrismaClient} from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+// OnModuleInit 和 OnModuleDestroy 是 NestJS 的生命周期钩子
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  // 在应用启动时连接数据库
+  async onModuleInit() {
+    await this.$connect();
+  }
+  // 在应用关闭时关闭数据库连接
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
