8ab6ebf0a8b634ca8de7f66e2b221ae02cdce464
parkourlele <2530778901@qq.com>
2025-10-14 10:19:21 +0800

    解决跨域

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  }); // 允许所有来源的跨域请求
   await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
